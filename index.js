53e0f5436c5f4e658e63030b3da4aaea91086b79
Shauna Empringham <shauna.empringham@gmail.com>
2022-11-06 11:44:46 -0500

    adding getSession helper function to get Last.fm session credentials

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-import dz from 'https://e-cdn-files.dzcdn.net/js/min/dz.js';
+import Bottleneck from 'bottleneck';
+import fetch from 'node-fetch';
+import fs from 'fs';
+import getSession from './helpers/getSession.js';
 
-console.log(dz);
+const limiter = new Bottleneck({
+	reservoir: 250,
+	reservoirRefreshAmount: 250,
+	reservoirRefreshInterval: 25 * 250,
+
+	maxConcurrent: 50,
+	minTime: 0,
+});
+
+const alt_api_url = 'https://ws.audioscrobbler.com/2.0/';
+const api_url = 'https://www.last.fm/api/';
+
+const session = await getSession();
+console.log(session);
